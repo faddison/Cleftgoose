d70534eb628708c4150ea7d41527b6e079d387d2
fraser addison <fraseraddison@gmail.com>
2013-05-07 20:38:48 -0700

    integrated profiles

---
+++ logs/jcontroller.log.php
@@ -0,0 +1,24 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2013-04-06 02:59:24 UTC
+#Software: Joomla Platform 12.2.0 Stable [ Neil Armstrong ] 21-September-2012 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2013-04-06T02:59:24+00:00	INFO	controller	Holding edit ID com_modules.edit.module.101 Array (     [0] => 101 ) 
+2013-04-06T02:59:25+00:00	INFO	controller	Checking edit ID com_modules.edit.module.101: 1 Array (     [0] => 101 ) 
+2013-04-06T02:59:35+00:00	INFO	controller	Checking edit ID com_modules.edit.module.101: 1 Array (     [0] => 101 ) 
+2013-04-06T02:59:35+00:00	INFO	controller	Holding edit ID com_modules.edit.module.101 Array (     [0] => 101 ) 
+2013-04-06T02:59:37+00:00	INFO	controller	Checking edit ID com_modules.edit.module.101: 1 Array (     [0] => 101 ) 
+2013-04-06T02:59:51+00:00	INFO	controller	Checking edit ID com_modules.edit.module.101: 1 Array (     [0] => 101 ) 
+2013-04-06T02:59:51+00:00	INFO	controller	Releasing edit ID com_modules.edit.module.101 Array ( ) 
+2013-04-06T03:00:01+00:00	INFO	controller	Holding edit ID com_modules.edit.module.88 Array (     [0] => 88 ) 
+2013-04-06T03:00:02+00:00	INFO	controller	Checking edit ID com_modules.edit.module.88: 1 Array (     [0] => 88 ) 
+2013-04-06T03:00:22+00:00	INFO	controller	Checking edit ID com_modules.edit.module.88: 1 Array (     [0] => 88 ) 
+2013-04-06T03:00:22+00:00	INFO	controller	Releasing edit ID com_modules.edit.module.88 Array ( ) 
+2013-04-06T03:00:33+00:00	INFO	controller	Holding edit ID com_modules.edit.module.101 Array (     [0] => 101 ) 
+2013-04-06T03:00:35+00:00	INFO	controller	Checking edit ID com_modules.edit.module.101: 1 Array (     [0] => 101 ) 
+2013-04-06T03:01:11+00:00	INFO	controller	Checking edit ID com_modules.edit.module.101: 1 Array (     [0] => 101 ) 
+2013-04-06T03:01:11+00:00	INFO	controller	Holding edit ID com_modules.edit.module.101 Array (     [0] => 101 ) 
+2013-04-06T03:01:13+00:00	INFO	controller	Checking edit ID com_modules.edit.module.101: 1 Array (     [0] => 101 ) 
+2013-04-06T03:02:17+00:00	INFO	controller	Checking edit ID com_modules.edit.module.101: 1 Array (     [0] => 101 ) 
+2013-04-06T03:02:17+00:00	INFO	controller	Releasing edit ID com_modules.edit.module.101 Array ( ) 
